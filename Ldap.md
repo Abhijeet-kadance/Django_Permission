72ca4ae70b7c47853bb511ff91358be477d67d0f
Abhijeet-kadance <Abhijeet.kadance.96@gmail.com>
2023-04-24 17:28:04 +0530

    Added restricted permissions to staff user

---
+++ Ldap.md
@@ -0,0 +1,5 @@
+## LDAP Django Integration
+
+> Library ( django python3 ldap)
+
+URL and Search Base is required
--- inventory/admin.py
+++ inventory/admin.py
@@ -1,26 +1,46 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group
 
 
-class ReadOnlyAdminMixin:
-    def has_add_permission(self, request):
-        return False
-    
-        
-    def has_change_permission(self, request, obj=None):
-        return True
-    
-        
-    def has_delete_permission(self, request, obj=None):
-        return False
-    
-        
-    def has_view_permission(self, request, obj=None):
-        return True
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
-@admin.register(Product)
-class ProductAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
-    list_display = ("name",)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    def get_form(self,request,obj=None, **kwargs):
+        form = super().get_form(request,obj,**kwargs)
+        is_superuser = request.user.is_superuser
+
+        if not is_superuser:
+            form.base_fields['username'].disabled = True
+            form.base_fields['is_superuser'].disabled = True
+            form.base_fields['user_permissions'].disabled = True
+            form.base_fields['groups'].disabled = True
+        return form
+
+
+# class ReadOnlyAdminMixin:
+#     def has_add_permission(self, request):
+#         return False
+    
+#     def has_change_permission(self, request, obj=None):
+#         if request.user.has_perm('inventory.change_product'):
+#             return False
+#         else:
+#             return False
+    
+#     def has_delete_permission(self, request, obj=None):
+#         return False
+        
+#     def has_view_permission(self, request, obj=None):
+#         return True
+
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ("name",)
 
     # def get_form(self,request,obj=None, **kwargs):
     #     form = super().get_form(request,obj,**kwargs)
@@ -32,3 +52,23 @@ class ProductAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     
     
     
+
+# class MyAuthenticationBackend:
+
+#     def authenticate(self, *args, **kwargs):
+#         pass
+    
+#     def get_user(self, user_id):
+#         try:
+#             return get_user_model().objects.get(pk=user_id)
+#         except get_user_model().DoesNotExist:
+#             return None
+
+#     def has_perm(self, user_obj, perm, obj=None):
+#         if perm == "view_category":
+#             return True # everybody can view
+#         # otherwise only the owner or the superuser can delete
+#         return user_obj.is_active and obj.user.pk==user_obj.pk
+
+#     def has_perms(self, user_obj, perm_list, obj=None):
+#         return all(self.has_perm(user_obj, perm, obj) for perm in perm_list)
