d776b04dbd30cf01e0f10f4a5e996fc551f6e690
Abhijeet-kadance <Abhijeet.kadance.96@gmail.com>
2023-04-24 14:52:23 +0530

    Created Read Only Admin MIxin

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,14 +2,33 @@ from django.contrib import admin
 from .models import Product
 
 
+class ReadOnlyAdminMixin:
+    def has_add_permission(self, request):
+        return False
+    
+        
+    def has_change_permission(self, request, obj=None):
+        return True
+    
+        
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+        
+    def has_view_permission(self, request, obj=None):
+        return True
+
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     list_display = ("name",)
 
-    def get_form(self,request,obj=None, **kwargs):
-        form = super().get_form(request,obj,**kwargs)
-        is_superuser = request.user.is_superuser
+    # def get_form(self,request,obj=None, **kwargs):
+    #     form = super().get_form(request,obj,**kwargs)
+    #     is_superuser = request.user.is_superuser
 
-        if not is_superuser:
-            form.base_fields['name'].disabled = True
-        return form
+    #     if not is_superuser:
+    #         form.base_fields['name'].disabled = True
+    #     return form
+    
+    
+    
